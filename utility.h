76bb3ee32548b02a0a041e1103545ffbbb0cbe74
tiehuis <marctiehuis@gmail.com>
2015-12-16 19:30:15 +1300

    Add generic vector type

---
+++ utility.h
@@ -0,0 +1,27 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <stdlib.h>
+
+#define xmalloc(x)              \
+({                              \
+    void *__mem = malloc(x);    \
+    if (!__mem) abort();        \
+    __mem;                      \
+})
+
+#define xrealloc(p, x)          \
+({                              \
+    void *__mem = realloc(p, x);\
+    if (!__mem) abort();        \
+    __mem;                      \
+})
+
+#define xcalloc(x)              \
+({                              \
+    void *__mem = calloc(x, 1); \
+    if (!__mem) abort();        \
+    __mem;                      \
+})
+
+#endif
---
+++ vector.h
@@ -0,0 +1,52 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdlib.h>
+#include "compiler.h"
+
+#define vec_declare(type, typename)                                          \
+                                                                             \
+typedef struct {                                                             \
+    type *data;                                                              \
+    size_t alloc;                                                            \
+    size_t len;                                                              \
+} vec_##typename##_t;                                                        \
+                                                                             \
+const size_t vec_##typename##_initial_alloc = 128;                           \
+                                                                             \
+vec_##typename##_t* vec_##typename##_init(void)                              \
+{                                                                            \
+    vec_##typename##_t *v = xmalloc(sizeof(vec_##typename##_t));             \
+    v->alloc = vec_##typename##_initial_alloc;                               \
+    v->data  = xmalloc(v->alloc * sizeof(type));                             \
+    v->len   = 0;                                                            \
+    return v;                                                                \
+}                                                                            \
+                                                                             \
+void vec_##typename##_free(vec_##typename##_t *v)                            \
+{                                                                            \
+    free(v->data);                                                           \
+    free(v);                                                                 \
+}                                                                            \
+                                                                             \
+static void __vec_##typename##_try_resize(vec_##typename##_t *v)             \
+{                                                                            \
+    if (v->len + 1 <= v->alloc)                                              \
+        return;                                                              \
+                                                                             \
+    v->alloc *= 2;                                                           \
+    v->data  = xrealloc(v->data, v->alloc * sizeof(type));                   \
+}                                                                            \
+                                                                             \
+void vec_##typename##_push(vec_##typename##_t *v, const type elem)           \
+{                                                                            \
+    __vec_##typename##_try_resize;                                           \
+    v->data[v->len++] = elem;                                                \
+}                                                                            \
+                                                                             \
+int vec_##typename##_pop(vec_##typename##_t *v)                              \
+{                                                                            \
+    return v->data[--v->len];                                                \
+}
+
+#endif
